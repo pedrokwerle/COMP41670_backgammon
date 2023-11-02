9044b889b2b7ce57a60277460dabc5c635a4d632
pedrokwerle <pedro.kreutzwerle@ucdconnect.ie>
2023-11-02 16:44:50 +0000

    Implemented ascii art to the dice

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -1,12 +1,64 @@
 package model;
+import userInterface.AsciiArt;
+import userInterface.ColorsAscii;
+import userInterface.Displayable;
+
 import java.util.random.RandomGenerator;
 
-public class Dice {
+public class Dice implements Displayable {
     private final int NUMBER_OF_SIDES = 6;
+    int value;
+
+    public Dice(){
+        this.asciiArt = new AsciiArt("src/model/dice.txt");
+    }
 
     public int rollDice(){
-        return RandomGenerator.getDefault().nextInt(NUMBER_OF_SIDES)+1;
+        value =  RandomGenerator.getDefault().nextInt(NUMBER_OF_SIDES)+1;
+        return value;
+    }
+
+
+
+    //AsciiArt section
+
+    AsciiArt asciiArt;
+    @Override
+    public void renderArt() {
+        try {
+            this.asciiArt.loadArt();
+        }
+        catch (Exception e){
+            System.out.println("File read error");
+        }
+        int height = this.asciiArt.getUniqueArt().size();
+        int lenght;
+        char charc;
+        for (int row = 0; row < height; row++) {
+            lenght = this.asciiArt.getUniqueArt().get(row).size();
+            for (int col = 0; col < lenght; col++) {
+                charc = this.asciiArt.getUniqueArt().get(row).get(col).getCharacter();
+                switch (charc) {
+                    case 'V': // Rank modifier
+                        charc = (char) this.value;
+                        this.asciiArt.getUniqueArt().get(row).get(col).setCharacter(charc);
+                        this.asciiArt.getUniqueArt().get(row).get(col).setColor(ColorsAscii.WHITE);
+                        break;
+                    default:
+                        this.asciiArt.getUniqueArt().get(row).get(col).setColor(ColorsAscii.WHITE);
+                        break;
+                }
+            }
+
+        }
+    }
+
+
+    @Override
+    public AsciiArt getArt() {
+        return null;
     }
 }
+
 
 
