2e24b65a29d9025b4a30d0589d5af284b666e339
tomas <tomas.sarapas@ucdconnect.ie>
2023-11-02 16:16:33 +0000

    added dice rolling methods to player object

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,6 +1,11 @@
 package model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Player {
+    private final ArrayList<Integer> rolls = new ArrayList<Integer>();
+    Dice playerDice = new Dice();
     private String playerName;
     public Player(String playerName){
         this.playerName = playerName;
@@ -12,5 +17,17 @@ public class Player {
 
     public String getPlayerName(){
         return this.playerName;
+    }
+
+    public int roll1(){
+        return this.playerDice.rollDice();
+    }
+
+    public ArrayList<Integer> rollMoves(){
+        this.rolls.clear();
+        this.rolls.add(this.playerDice.rollDice());
+        this.rolls.add(this.playerDice.rollDice());
+
+        return rolls;
     }
 }
