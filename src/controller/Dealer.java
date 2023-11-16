fe0d86589d1cc1c10c24107b713327a643acd8d9
pedrokwerle <pedro.kreutzwerle@ucdconnect.ie>
2023-11-16 16:04:35 +0000

    Dealer class can now move checkers between lanes

---
+++ src/controller/Dealer.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.BackgammonTable;
+import model.Checker;
+import model.MovesLog;
+
+public class Dealer {
+    MovesLog log;
+    BackgammonTable table;
+     public Dealer(BackgammonTable table){
+         this.log = new MovesLog();
+         this.table = table;
+     }
+
+     public void moveChecker(int fromLaneNum, int toLaneNum){
+         // lane numbering goes A->0, B->1,...Y->23
+         Checker checker = table.getLanes().get(fromLaneNum).removeChecker();
+         table.getLanes().get(toLaneNum).addChecker(checker);
+     }
+
+
+}
