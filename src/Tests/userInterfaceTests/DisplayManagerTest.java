0642bd96062781bb25de0c88199f30ee037c460e
tomas <tomas.sarapas@ucdconnect.ie>
2023-12-10 23:34:05 +0000

    dispMan tests

--- src/Tests/userInterfaceTests/DisplayManagerTest.java
+++ src/Tests/userInterfaceTests/DisplayManagerTest.java
@@ -1,6 +1,12 @@
 package Tests.userInterfaceTests;
 
+import model.Checker;
 import org.junit.jupiter.api.Test;
+import userInterface.ColorsAscii;
+import userInterface.DisplayManager;
+import userInterface.Displayable;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,16 +14,14 @@ class DisplayManagerTest {
 
     @Test
     void addToCache() {
+        Checker testChecker = new Checker(ColorsAscii.WHITE);
+        DisplayManager dispMan = new DisplayManager(10, 10);
+        dispMan.addToCache(testChecker, 0 ,0);
+        ArrayList<Displayable> dispTest = dispMan.getRenderCache();
+        assertEquals(testChecker, dispTest.get(0));
+        dispMan.clearCache();
+        assertEquals(dispTest.size(), 0);
     }
-
-    @Test
-    void testAddToCache() {
-    }
-
-    @Test
-    void clearCache() {
-    }
-
     @Test
     void renderObject() {
     }
